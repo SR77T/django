f8340b8f2eec425ba4a36ab4406e9363c0094fa9
SR77T <sarthakrasaily@gmail.com>
2024-04-23 19:24:52 +0545

    template_project

--- myapp/views.py
+++ myapp/views.py
@@ -4,5 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello world. I am learning django")
-    
+    query_strings = request.GET
+    name = query_strings.get("name")
+    return HttpResponse(f"Hello world. I am {name}")
+
+def home_detail(request, id):
+    return HttpResponse(f"My home id is {id}")
+
+def portfolio(request):
+    return render(request, template_name="myapp/portfolio.html")
